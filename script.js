d78c893f5349fa94ca16a28f2e3a3695ca72a37e
ferreiradev7 <humbertopferreiraf@gmail.com>
2025-10-16 20:16:39 -0300

    feat: menu hamburguer mobile

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ const elementsToAnimate = document.querySelectorAll('.feature-card, .plano-card'
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            
+
             entry.target.classList.add('visible');
         }
     });
 }, {
-    threshold: 0.1 
+    threshold: 0.1
 });
 
 
 elementsToAnimate.forEach(element => {
     observer.observe(element);
+});
+
+
+const hamburger = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.main-nav');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('is-active');
+});
+
+document.querySelectorAll('.main-nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('is-active');
+    });
 });
