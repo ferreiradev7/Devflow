8d27fae6f40713c6a4e5e32e48fae143bd9ed0d7
ferreiradev7 <humbertopferreiraf@gmail.com>
2025-10-17 16:27:40 -0300

    mobile att

--- script.js
+++ script.js
@@ -23,10 +23,12 @@ const navMenu = document.querySelector('.main-nav');
 
 hamburguer.addEventListener('click', () => {
     navMenu.classList.toggle('is-active');
+    hamburguer.classList.toggle('is-active');
 });
 
 document.querySelectorAll('.main-nav a').forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('is-active');
+        hamburguer.classList.remove('is-active');
     });
 });
